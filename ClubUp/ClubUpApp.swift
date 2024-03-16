f119a4581065a72123dd9a6a2ab1314b62c75b44
phillt3 <tracyphill3@gmail.com>
2024-03-16 17:24:36 -0400

    Initial Commit

---
+++ ClubUp/ClubUpApp.swift
@@ -0,0 +1,32 @@
+//
+//  ClubUpApp.swift
+//  ClubUp
+//
+//  Created by Phillip  Tracy on 3/16/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ClubUpApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
