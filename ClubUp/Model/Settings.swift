316453107128ac22c49ea9856f312d03c9bbab3f
phillt3 <tracyphill3@gmail.com>
2024-04-22 19:30:50 -0400

    Enums reworked, settings model added, inital settings view created.

---
+++ ClubUp/Model/Settings.swift
@@ -0,0 +1,8 @@
+//
+//  Settings.swift
+//  ClubUp
+//
+//  Created by Phillip  Tracy on 4/22/24.
+//
+
+import Foundation
---
+++ ClubUp/View/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  ClubUp
+//
+//  Created by Phillip  Tracy on 4/22/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
